cda95143ee1ac35e77dfecbc4dcc57ff717dd7bc
STD\s20217021 <20217021-matsuiren@hcs.ac.jp>
2023-01-27 12:27:44 +0900

    倉庫番音楽

---
+++ TITLEB.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TITLEB : MonoBehaviour
+{
+
+    [SerializeField, Header("飛ばすシーン名をいれる")] private string nextSceneName;
+
+    public void PressStart()
+    {
+         //ここに次のシーンへいく命令を書く
+         SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
+        Time.timeScale = 1.0f;
+    }
+
+    //ゲームプレイ終了
+    public void OnclickStarButtonExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    //ゲームプレイ終了
+    Application.Quit();
+#endif
+    }
+    
+}
